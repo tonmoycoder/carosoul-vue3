dc180ad4ed260fc5f454eb3dc0920f05ce4395c0
tonmoy <tanverislamtonmoy2929@gmail.com>
2023-02-16 18:18:23 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <Carousel :items-to-show="1" :wrap-around="true">
+      <Slide v-for="slide in 10" :key="slide">
+        <div class="carousel__item">{{ slide }}</div>
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+
+export default defineComponent({
+  name: "Basic",
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+});
+</script>
+
+<style>
+.wrapper {
+  max-width: 700px;
+  margin: 0 auto;
+}
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: #42b883;
+  color: brown;
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+  border-radius: 16px;
+  margin: -9px 22px;
+}
+</style>
